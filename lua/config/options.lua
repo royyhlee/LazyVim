983b471b621e4192b32d02ea9f76b8f569cfdfdc
Roy Lee <ylee@costar.com>
2025-06-12 10:53:21 -0700

    Added folding options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,11 @@ vim.opt.wrap = true
 
 vim.g.snacks_animate = false
 vim.g.autoformat = false
+
+vim.opt.foldcolumn = '1' -- '0' is not recommended
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 2
+vim.opt.foldenable = true
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
