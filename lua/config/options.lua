952b20015f9c9cf82492923f736e36e09272dca8
Roy Lee <ylee@costar.com>
2025-06-18 09:24:14 -0700

    Disable fold start

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,8 +11,8 @@ vim.g.autoformat = false
 
 vim.opt.foldcolumn = '1' -- '0' is not recommended
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 2
-vim.opt.foldenable = true
+-- vim.opt.foldlevelstart = 2
+vim.opt.foldenable = false
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
